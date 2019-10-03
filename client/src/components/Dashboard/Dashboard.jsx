06e71658fca4202a598052ef452e15a22a79d59f
Joshua Fuentes <joshuarpf@gmail.com>
2019-10-03 21:37:01 +0800

    Added icons

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -13,76 +13,72 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 // import Paper from '@material-ui/core/Paper';
 // import Link from '@material-ui/core/Link';
-// import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import useStyles from './Styles'
 
 export default function Dashboard() {
 	const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-//   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+	const [open, setOpen] = React.useState(true);
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            {/* <MenuIcon /> */}
-          </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            Dashboard
-          </Typography>
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-            </Badge>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<div className={classes.root}>
+		<CssBaseline />
+		<AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+			<Toolbar className={classes.toolbar}>
+			<IconButton
+				edge="start"
+				color="inherit"
+				aria-label="open drawer"
+				onClick={handleDrawerOpen}
+				className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+			>
+			<MenuIcon />
+			</IconButton>
+			<Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+				Dashboard
+			</Typography>
+			</Toolbar>
+		</AppBar>
+		<Drawer
+			variant="permanent"
+			classes={{
+			paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+			}}
+			open={open}
+		>
+			<div className={classes.toolbarIcon}>
+			<IconButton onClick={handleDrawerClose}>
+			</IconButton>
+			</div>
+			<Divider />
+			<List>
+				<ol>
+					<li>Something</li>
+				</ol>
+			</List>
+			<Divider />
+			<List>
 			<ol>
-				<li>Something</li>
-			</ol>
-		</List>
-        <Divider />
-        <List>
-		<ol>
-				<li>Something</li>
-			</ol>
-		</List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-          </Grid>
-        </Container>
-        {/* <Copyright /> */}
-      </main>
-    </div>
-  );	
+					<li>Something</li>
+				</ol>
+			</List>
+		</Drawer>
+		<main className={classes.content}>
+			<div className={classes.appBarSpacer} />
+			<Container maxWidth="lg" className={classes.container}>
+			<Grid container spacing={3}>
+			</Grid>
+			</Container>
+			{/* <Copyright /> */}
+		</main>
+		</div>
+	);	
 }
