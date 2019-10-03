e02c8a09ee5ac1521b2a942a2867a1c0a6e24b9d
Joshua Fuentes <joshuarpf@gmail.com>
2019-10-03 12:52:07 +0800

    Added entities route

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express, { Router } from 'express'
 
 // Routes
 import indexRoute from './src/api/routes'
+import EnttiTiesRoute from './src/api/routes/Entities'
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/', indexRoute)
+app.use('/entities', EnttiTiesRoute);
 
 app.listen(PORT, () => { 
 	console.log(`Server is running on port : ${PORT}`)
